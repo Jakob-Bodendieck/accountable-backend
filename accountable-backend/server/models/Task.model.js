235989f731b2f3dd770959555d820daab65f4416
Jakob Bodendieck <bodendieck.jakob@gmail.com>
2023-11-29 12:40:56 +0000

    task routes setup

---
+++ accountable-backend/server/models/Task.model.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      unique: [true, "A task with this name already exists"]
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    deadline: {
+        type: Date,
+    },
+    attachments: {
+        type: String,
+    }
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Task = model("Task", taskSchema);
+
+module.exports = Task;
---
+++ accountable-backend/server/routes/task.routes.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const router  = express.Router()
+
+const Task  = require('../models/Task.model')
+
+/*Routes */
+
+//Post - Create a new task
+router.post("/task", (req, res)=>{ //do we need to add /:userId/task instead of just /task? 
+    const {title, description, deadline, attachments} = req.body;
+
+    Task.create({title, description, deadline, attachments}) //add array of users here when adding friends functionality. 
+    .then((response) => res.json(response))
+    .catch((error)=> res.json(error))
+})
+
+//Get - Reads all tasks
+
+router.get("/tasks", (req, res)=>{
+    Task.find()
+    .then((allTasks) => res.json (allTasks))
+    .catch((error)=>res.json(error))
+})
+
+//Get - Reads one specific task
+router.get("/tasks/:taskId", (req, res)=>{
+    const {taskId} = req.params;
+    Task.findById(taskId)
+    .then((task)=> res.json(task))
+    .catch((error)=>res.json(error))
+})
+
+//Put - Updates one specific task
+router.put("/tasks/:taskId", (req, res)=>{
+    const {taskId} = req.params;
+    const {title, description, deadline, attachments} = req.body;
+
+    Task.findByIdAndUpdate(taskId, {title, description, deadline, attachments}, {new: true})
+    .then(()=>{
+        res.json({message: "Task Updated!"})
+    })
+    .catch ((error)=>{
+        res.json({message: "Task failed to update!"})
+    })
+})
+
+//Delete - Deletes one specific task
+router.delete("/tasks/:taskId", (req, res)=>{
+    const {taskId} = req.params;
+    Task.findByIdAndDelete(taskId)
+    .then(()=>{
+        res.json({message: "Task deleted"})
+    })
+    .catch((error)=>{
+        res.json({message: "Task could not be deleted"})
+    })
+})
+
+module.exports = router;
